2e9ac507d50b3022dd1136eca8d0d24064583da8
geoffroyBel <rastakongoner@gmail.com>
2024-08-20 12:13:13 +0200

    last geof nostr

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,6 +1,6 @@
 
-
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { SimplePool } from 'nostr-tools/pool'
 import { generateSecretKey, getPublicKey, finalizeEvent } from 'nostr-tools/pure';
 import { Relay } from 'nostr-tools/relay';
 import { bytesToHex, hexToBytes } from '@noble/hashes/utils';
@@ -28,15 +28,18 @@ const { sk, pk } = loadOrGenerateKeys();
 const getColorFromPubkey = (pubkey) => {
     const hash = CryptoJS.SHA256(pubkey).toString(); // Hash de la clé publique
     return `#${hash.slice(0, 6)}`;
-};
+  };
 
-const userColor = getColorFromPubkey(pk);
+  const userColor = getColorFromPubkey(pk);
 
 const Grid = ({ size, username }) => {
+  let relays = ['wss://relay.example.com', 'wss://relay.example2.com']
+  const pool = new SimplePool()
     const [relay, setRelay] = useState();
     const [pixels, setPixels] = useState(
         Array(size).fill().map(() => Array(size).fill({ color: '#FFFFFF', timestamp: 0 }))
     );
+    const timeoutRef = useRef(null);
     const [playerCells, setPlayerCells] = useState(() => {
       // Charger le nombre de cases remplies depuis le Local Storage
       return parseInt(localStorage.getItem('playerCells'), 10) || 0;
@@ -45,7 +48,7 @@ const Grid = ({ size, username }) => {
     useEffect(() => {
         const connectRelay = async () => {
             try {
-                const _relay = await Relay.connect('wss://relay.damus.io');
+                const _relay = await Relay.connect('wss://longhorn.bgp.rodeo');
                 _relay.subscribe(
                     [
                         {
@@ -81,34 +84,48 @@ const Grid = ({ size, username }) => {
         connectRelay();
     }, []);
 
+
+
     const handlePixelClick = async (x, y) => {
-        const timestamp = Math.floor(Date.now() / 1000);
-        const newPixels = [...pixels];
-        newPixels[x][y] = { color: userColor, timestamp };
-        setPixels(newPixels);
-        setPlayerCells(prev => {
-          const newCount = prev + 1;
-          localStorage.setItem('playerCells', newCount); // Sauvegarder dans le localStorage
-          return newCount;
-        });
-        const eventTemplate = {
-            pubkey: pk,
-            created_at: timestamp,
-            kind: 1,
-            tags: [["t", "cesipixelwar"]],
-            content: JSON.stringify({ x, y, color: newPixels[x][y].color }),
-        };
-
-        const signedEvent = finalizeEvent(eventTemplate, sk);
-        console.log(signedEvent);
-
-        try {
-            await relay.publish(signedEvent);
-            console.log('Event published:', signedEvent);
-        } catch (err) {
-            console.error('Failed to publish event:', err);
-        }
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+         timeoutRef.current = setTimeout(async () => {
+          const timestamp = Math.floor(Date.now() / 1000)
+            const newPixels = [...pixels];
+            newPixels[x][y] = {color: userColor, timestamp}; // Par exemple, couleur choisie pour la coloration
+            setPixels(newPixels);
+            setPlayerCells(prev => {
+              const newCount = prev + 1;
+              localStorage.setItem('playerCells', newCount); // Sauvegarder dans le localStorage
+              return newCount;
+            });
+            
+            const eventTemplate = {
+              pubkey: pk,
+              created_at: Math.floor(Date.now() / 1000),
+              kind: 1,
+              tags: [
+                ["t", "cesipixelwar"] // Correct format for a tag
+              ],
+              content: JSON.stringify({ x, y, color: newPixels[x][y].color }),
+            };
+          
+           
+            
+            const signedEvent = finalizeEvent(eventTemplate, sk);
+            console.log(signedEvent)
+            
+            try {
+              await relay.publish(signedEvent);
+              console.log('Event published:', signedEvent);
+            } catch (err) {
+              console.error('Failed to publish event:', err);
+            }
+            timeoutRef.current = null;
+          }, 10); 
     };
+
 
     return (
         
